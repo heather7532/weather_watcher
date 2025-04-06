aa8f0d946e7358b459b30154fc1491056174d030
Heather <heather.e.gulledge@gmail.com>
2025-04-05 21:48:41 -0500

    fixed example

--- example/weather_watcher_example.dart
+++ example/weather_watcher_example.dart
@@ -1,6 +1,20 @@
 import 'package:weather_watcher/weather_watcher.dart';
 
-void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
+void main() async {
+  final weatherUpdater = WeatherUpdater(
+    apiKey: 'YOUR_API_KEY_HERE', // Replace with your actual OpenWeatherMap API key
+    onWeatherUpdate: (WeatherData data) {
+      print('Temperature: ${data.temp} ${data.unitLabel}');
+      print('Humidity: ${data.humidity}%');
+    },
+  );
+
+// Start periodic weather updates
+  weatherUpdater.start();
+
+// Let it run for one cycle for this example
+  await Future.delayed(Duration(seconds: 20));
+
+// Stop updater to clean up
+  weatherUpdater.stop();
 }
--- weather_watcher.dart
+++
@@ -1,4 +0,0 @@
-/// Developed by Alpenlogic LLC
-
-export 'src/updater.dart';
-export 'src/weather_source.dart';
